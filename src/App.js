40939bce63a543cadfbed1632c469808d2ea9687
mayday <maydaythecoder@gmail.com>
2023-08-21 16:06:03 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 import './App.css';
+import Grid from './Components/Grid';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-  </div>
+      <Grid />
+    </div>
   );
 }
 
